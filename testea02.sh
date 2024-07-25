61252e794126f50058281cfbcb94fd70c438866b
Warlei-C-Moreira <96666619+Warlei-C-Moreira@users.noreply.github.com>
2024-07-25 10:39:08 -0300

    Upload do nosso segundo script em Shell
    Upload de arquivos no Github

---
+++ testea02.sh
@@ -0,0 +1,8 @@
+<Add file>
+	Upload files
+		Or choose your files: 
+			Envio de arquivos: teste02.sh <Abrir>
+		Commit changes: Upload do nosso segundo script em Shell
+		Add and optional: Upload de arquivos no Github
+		Check: Commit directly to the master branch
+<Commit changes>
